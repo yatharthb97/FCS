78260f7d84455911fcbf4fe89027668360f64347
Yatharth Bhasin <yatharth1997+git@gmail.com>
2021-05-25 22:03:11 +0530

    Added Profile() and resolved macro modes

--- LangevinSim/particle.hpp
+++ LangevinSim/particle.hpp
@@ -11,24 +11,25 @@ class Particle
 public:
 
 	unsigned int partid;
-	V pos; 		         	//Position Vector
-   	V init_pos;          	//Initial Position
-	double D = 3.0;      	//Diffusion Rate
+	V pos; 		         	    //Position Vector
+  V init_pos;          	  //Initial Position
+	double D = 3.0;      	  //Diffusion Rate
 	double qm_yield = 0.8; 	//Emmission base probablity
 	bool InVol = false;     // Is the particle in the observation Volume ?
 	
 	unsigned long int last_flash = 0;  //Time of last flash
 	//unsigned long int creation_t = 0;  //Time of Creation
 	
-	unsigned int FlashCount;  //Number of times the molecule is flashed
-	bool bleached = false;    //Is the molecule bleached?
+	#if FCS_BLEACHING == 1
+  	bool bleached = false;    //Is the molecule bleached?
+  	unsigned int bleach_after;
+  #endif
 
-	unsigned int bleach_after;
 
-
-	#if defined(FCS_PER_PART_STATS) //Calculate stats for every Particle
+	#if FCS_PER_PART_STATS == 1 //Calculate stats for every Particle
 		double dist = 0;
 		double dist_sq = 0;
+    unsigned int FlashCount;  //Number of times the molecule is flashed
 	#endif
 
 
@@ -69,8 +70,38 @@ public:
   	#endif
   } // End of Flash()
 
+
+
+/*  //Outputs a string → Beautified and labelled
+  void __attribute__((always_inline)) Profile()
+  {
+
+  }*/
+
+
+  //Friend Declarations
+  friend std::ostream& operator<< (std::ostream &stream, const Particle &part);
+
+
 }; //End of class Particle
-//static Particle::bleach_after = 5;
+
+
+
+//! Stream operator overload that puts Particle info to the ostream.
+std::ostream& operator<< (std::ostream &stream, const Particle &part)
+{
+   stream << part.partid << FCS_DSep << part.D << FCS_DSep 
+          << part.qm_yield;
+
+   #if FCS_PER_PART_STATS == 1
+    stream << FCS_DSep << part.dist << FCS_DSep <<  part.dist_sq << '\n';
+   #else
+    stream << '\n';
+  #endif
+
+  return stream;
+
+} //End of friend overload operator<<
 
 
 //Typename aliases
