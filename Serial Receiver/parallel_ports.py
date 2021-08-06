ee6f48d6cb31952d746d1b38d651438427e5d477
Yatharth <yatharth1997@gmail.com>
2021-08-06 14:08:40 +0530

    Updated with new API

--- Serial Receiver/parallel_ports.py
+++ Serial Receiver/parallel_ports.py
@@ -4,14 +4,11 @@
 
 
 from receiver import Receiver, ListPorts
+from acquisition_fns import Events_Acq, Time_Acq
+from receive_handlers import *
 
+#List All Ports
 ListPorts()
-print("\n\n")
-
-session = "Test1"
-print(f"Session → Test1")
-
-print("\n\n")
 
 #Initalise ports
 ports = [Receiver("Port 1", "COM8", Baudrate = 115200, Sep = ','),
@@ -20,31 +17,5 @@ ports = [Receiver("Port 1", "COM8", Baudrate = 115200, Sep = ','),
 		 Receiver("Port 4", "COM11", Baudrate = 115200, Sep = ',')]
 
 
-#Set file and max counter values
-for port in ports:
-	filename = "./" + session + '_' + port.Name + ".txt"
-	port.to_file(filename)
-	port.set_down_counter(10000);
+Events_Acq("Test1", ports, receive_optm, 10000)
 
-#Open Ports
-for port in ports:
-	port.open()
-
-#Check if the ports are open
-open_status = sum([port.is_open() for port in ports])
-
-#If ports are open → Read sequentially from each port
-if(open_status == len(ports)):
-    for port in ports:
-        if port.EventCounter > 0:
-        	port.receive_to_file()
-else:
-	print(" • ERROR > Some ports are not open!")
-
-#Close ports
-for port in ports:
-	port.close()
-
-#Print status of ports
-for port in ports:
-	port.status()
