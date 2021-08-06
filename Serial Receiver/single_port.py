507a281ebf4283b58a1bbe00b15072068c2ad14a
Yatharth <yatharth1997@gmail.com>
2021-08-06 14:10:20 +0530

    Updated Single port file with new API

--- Serial Receiver/single_port.py
+++ Serial Receiver/single_port.py
@@ -3,23 +3,15 @@
 #Author - Yatharth Bhasin (github → yatharthb97)
 
 from receiver import Receiver, ListPorts
+from acquisition_fns import Events_Acq, Time_Acq
+from receive_handlers import *
+
+#List All Ports
+#ListPorts()
+
+#Initalise ports
+ports_x = [Receiver("Teensy Output", "COM8", Baudrate = 115200, Sep = ',')]
+#ports = Receiver("Teensy Output", "COM8", Baudrate = 115200, Sep = ',')
 
 
-ListPorts()
-print("\n\n")
-
-rnd_port = Receiver("Teensy Rnd Output", "COM8", 19200, Sep = ',')
-
-
-rnd_port.to_file("./test18.txt")
-rnd_port.set_down_counter(10000);
-rnd_port.open()
-
-if(rnd_port.is_open()):
-    while(rnd_port.EventCounter > 0):
-        rnd_port.receive_to_file()
-   
-
-rnd_port.status()
-rnd_port.close()
-rnd_port.File.close()
+ports_x = Events_Acq("Test1", ports_x, receive_with_time_print, 1000)
